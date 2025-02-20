82627f72dba75f6ab6e7ee144e2cad403080e0e7
Augusto Sopelsa Klaic <ntconsult.augusto@agi.com.br>
2025-02-20 16:17:03 -0300

    feat(elk-basics): init project folder of the elk stack

---
+++ elk-basics/src/main/java/com/augusto/elkbasics/ElkBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.augusto.elkbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElkBasicsApplication.class, args);
+    }
+
+}
