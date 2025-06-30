7ca380bf087a653a611b8f479428ac91268e468c
Augusto Sopelsa Klaic <ntconsult.augusto@agi.com.br>
2025-06-30 11:47:09 -0300

    feat(graphql-basics): done hello world graphql

---
+++ graphql-basics/graphql-server/src/main/java/com/augusto/graphqlbasics/graphqlserver/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.augusto.graphqlbasics.graphqlserver.controller;
+
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+
+    @QueryMapping
+    public String hello() {
+        return "Hello GraphQL!";
+    }
+}
