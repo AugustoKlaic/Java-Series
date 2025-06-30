439bb2f454d90b748381b5cfffe91886032eb27c
Augusto Sopelsa Klaic <ntconsult.augusto@agi.com.br>
2025-06-30 11:37:08 -0300

    feat(graphql-basics): done initial graphql project structure

---
+++ graphql-basics/graphql-info-provider-A/src/main/java/com/augusto/graphqlbasics/infoproviderA/GraphqlInfoProviderAApplication.java
@@ -0,0 +1,13 @@
+package com.augusto.graphqlbasics.infoproviderA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlInfoProviderAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlInfoProviderAApplication.class, args);
+    }
+
+}
---
+++ graphql-basics/graphql-info-provider-B/src/main/java/com/augusto/graphqlbasics/infoproviderB/GraphqlInfoProviderBApplication.java
@@ -0,0 +1,13 @@
+package com.augusto.graphqlbasics.infoproviderB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlInfoProviderBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlInfoProviderBApplication.class, args);
+    }
+
+}
