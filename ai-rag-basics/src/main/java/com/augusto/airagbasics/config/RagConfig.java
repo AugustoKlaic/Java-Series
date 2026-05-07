2d566d8a9dc07c072654d972f466b8dd91ce916b
Augusto Sopelsa Klaic <ntconsult.augusto@agi.com.br>
2026-05-07 17:41:18 -0300

    feat(airagbasics): done configuration and ingestion service

--- ai-rag-basics/src/main/java/com/augusto/airagbasics/config/RagConfig.java
+++ ai-rag-basics/src/main/java/com/augusto/airagbasics/config/RagConfig.java
@@ -1,4 +1,63 @@
 package com.augusto.airagbasics.config;
 
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiEmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.qdrant.QdrantEmbeddingStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RagConfig {
+
+    @Value("${spring.ai.openai.api-key}")
+    private String apiKey;
+
+    @Value("${qdrant.host}")
+    private String qdrantHost;
+
+    @Value("${qdrant.port}")
+    private Integer qdrantPort;
+
+    @Value("${qdrant.collection}")
+    private String qdrantCollection;
+
+    /*
+        for understanding language and generating responses
+     */
+    @Bean
+    public OpenAiChatModel chatModel() {
+        return OpenAiChatModel.builder()
+                .apiKey(apiKey)
+                .baseUrl("https://openrouter.ai/api/v1")
+                .modelName("meta-lamma/llama-3.1-8b-instruct:free")
+                .build();
+    }
+
+    /*
+        to convert text into numeric vectors
+     */
+    @Bean
+    public EmbeddingModel embeddingModel() {
+        return OpenAiEmbeddingModel.builder()
+                .apiKey(apiKey)
+                .baseUrl("https://openrouter.ai/api/v1")
+                .modelName("text-embedding-ada-002")
+                .build();
+    }
+
+    /*
+        to store and retrieve the vectors
+     */
+    @Bean
+    public EmbeddingStore<TextSegment> embeddingStore() {
+        return QdrantEmbeddingStore.builder()
+                .host(qdrantHost)
+                .port(qdrantPort)
+                .collectionName(qdrantCollection)
+                .build();
+    }
 }
--- ai-rag-basics/src/main/java/com/augusto/airagbasics/ingestion/DocumentIngestionService.java
+++ ai-rag-basics/src/main/java/com/augusto/airagbasics/ingestion/DocumentIngestionService.java
@@ -1,4 +1,49 @@
 package com.augusto.airagbasics.ingestion;
 
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.Path;
+import java.util.List;
+
+@Service
 public class DocumentIngestionService {
+
+    private static final Integer CHUNK_SIZE = 500;
+    private static final Integer CHUNK_OVERLAP = 50;
+
+    private final EmbeddingModel embeddingModel;
+    private final EmbeddingStore<TextSegment> embeddingStore;
+
+    private final String documentPath;
+
+
+    public DocumentIngestionService(EmbeddingModel embeddingModel,
+                                    EmbeddingStore<TextSegment> embeddingStore,
+                                    @Value("${documents.path}") String documentPath) {
+        this.embeddingModel = embeddingModel;
+        this.embeddingStore = embeddingStore;
+        this.documentPath = documentPath;
+    }
+
+    public void ingestDocument() {
+        List<Document> documents = FileSystemDocumentLoader.loadDocuments(Path.of(documentPath));
+
+        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
+                .documentSplitter(DocumentSplitters.recursive(CHUNK_SIZE, CHUNK_OVERLAP))
+                .embeddingModel(embeddingModel)
+                .embeddingStore(embeddingStore)
+                .build();
+
+        ingestor.ingest(documents);
+        System.out.println("Indexed " + documents.size() + " documents");
+    }
 }
