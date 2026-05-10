1e5b6e0287a4e0c142340d8c767d0d293ef6c915
Augusto Sopelsa Klaic <ntconsult.augusto@agi.com.br>
2026-05-09 22:43:33 -0300

    feat(ai-agents-basics): initial gradle structure done

---
+++ ai-agents-basics/src/main/java/com/augusto/aiagentsbasics/AiAgentsBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.augusto.aiagentsbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAgentsBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiAgentsBasicsApplication.class, args);
+    }
+
+}
