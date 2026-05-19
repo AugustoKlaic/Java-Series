b83831d78e68287dc2d3cdc78f0ebb2639d49817
AugustoKlaic <gutosopelsa@hotmail.com>
2026-05-18 22:29:42 -0300

    feat(kubernetes-basics): initial setup structure

---
+++ kubernetes-basics/src/main/java/com/augusto/kubernetesbasics/KubernetesBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.augusto.kubernetesbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesBasicsApplication.class, args);
+    }
+
+}
